75bff95d3584149c593d65b5a503e97f57670098
Matteo Ricci <mattrcc95@gmail.com>
2021-11-14 10:16:13 +0100

    introduced coroutines

---
+++ src/test/kotlin/com/example/SuspendExperiment/SuspendExperimentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SuspendExperiment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SuspendExperimentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
